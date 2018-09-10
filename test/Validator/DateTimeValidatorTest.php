30af67483c27c552b2067335a09b6a21ccd609d3
Mohos Tamás <tomi@mohos.name>
2018-09-10 13:21:17 +0200

    test(Validator): add more test cases

--- test/Validator/DateTimeValidatorTest.php
+++ test/Validator/DateTimeValidatorTest.php
@@ -14,21 +14,46 @@ class DateTimeValidatorTest extends TestCase {
 
     /**
      * @test
+     * @dataProvider invalidDateTimeProvider
      */
-    public function validate_givenInvalidDate_throwsExceptionWithErrorMessage() {
+    public function validate_givenInvalidDate_throwsExceptionWithErrorMessage($dateTime) {
         $validator = new DateTimeValidator();
         $this->expectException(ValidationException::class);
         $this->expectExceptionMessage('Wrong datetime format');
-        $validator->validate('wrong date format');
+        $validator->validate($dateTime);
+    }
+
+    public function invalidDateTimeProvider()
+    {
+        return [
+            [''],
+            ['wrong date format'],
+            ['a'],
+            ['2010.01.01 12:00:00'],
+            ['2010-01-01 12.00.00'],
+            ['2010-01-01 12'],
+            ['2010-01 12:00:00'],
+        ];
     }
 
 
     /**
      * @test
+     * @dataProvider validDateTimeProvider
      */
-    public function validate_givenValidEmail_returnsNull() {
+    public function validate_givenValidEmail_returnsNull($dateTime) {
         $emptyValidator = new DateTimeValidator();
-        $this->assertNull($emptyValidator->validate('2018-09-10 13:08'));
+        $this->assertNull($emptyValidator->validate($dateTime));
 
+    }
+
+    public function validDateTimeProvider()
+    {
+        return [
+            ['2010-01-01 12:00:00'],
+            ['2010-01-01 12:00'],
+            ['2010-1-1 12:00'],
+            ['2010-1-1 1:1:1']
+        ];
     }
 }
